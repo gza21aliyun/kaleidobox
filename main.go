b8765ef73dd3bfb745a6443f1421202eb3c70d01
osk666 <osk666@qq.com>
2026-02-07 12:50:32 +0800

    恢复ui设定

--- main.go
+++ main.go
@@ -126,8 +126,8 @@ func main() {
 			},
 		},
 		BackgroundColour: &options.RGBA{R: 18, G: 20, B: 22, A: 255},
-		StartHidden:      false,
-		Frameless:        false, // 启用无边框模式
+		StartHidden:      true,
+		Frameless:        true, // 启用无边框模式
 		// 样式完全交由wails前端控制
 		Windows: &windows.Options{
 			WebviewIsTransparent: true,
